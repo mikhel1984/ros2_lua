df84ed025882e99bb6ea8d71cac612bed6e39f06
Stanislav <mikhel.sk@gmail.com>
2024-08-29 20:50:59 +0300

    Update hooks

--- rcllua/rcllua/rcllua.lua
+++ rcllua/rcllua/rcllua.lua
@@ -1,14 +1,17 @@
 
+local rclbind = require("rclbind")
 rcllua = {}
 
 rcllua.init = function (args)
+  print("init")
 end
 
 rcllua.spin = function (node)
+  print("spin")
 end
 
 rcllua.shutdown = function (node)
-
+  print("node")
 end
 
 return rcllua

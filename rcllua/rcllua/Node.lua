6f39a38b8a78d7de05e46a1681b980bf1149b45a
Stas <mikhel.sk@gmail.com>
2024-12-15 18:55:40 +0300

    Update some Lua functions

--- rcllua/rcllua/Node.lua
+++ rcllua/rcllua/Node.lua
@@ -1,10 +1,84 @@
 
 local rclbind = require("rcllua.rclbind")
 
-Node = {}
 
-function Node._init (name, ns)
-  return rclbind.node_init(name, ns)
+local Logger = {name='rcllua'}
+Logger.__index = Logger
+
+-- local
+Node = {}
+Node.__index = Node
+
+function Node.create_publisher (self, ...)
+  return rclbind.new_publisher(self._node__object, ...)
+end
+
+function Node.create_timer (self, period)
+  return rclbind.new_timer(self._clock__object, period)
+end
+
+function Node.get_logger (self)
+  local o = {name=self._node__name}
+  return setmetatable(o, Logger)
+end
+
+local protected = {name=true, namespace=true, init=true}
+
+function Node.__call (self, ...)
+  local param = self._init__list
+  -- make instance
+  local o = {}
+  -- create node object
+  o._node__object = rclbind.new_node(param.name, param.namespace)
+  -- add default clock
+  o._clock__object = rclbind.new_clock()
+  -- save name for quick access
+  o._node__name = param.name
+  -- call initialization
+  if param.init then
+    param.init(self, ...)
+  end
+  -- copy other elements
+  for k, v in pairs(param) do
+    if not protected[k] then o[k] = v end
+  end
+  setmetatable(o, Node)
+  return o
+end
+
+local node_mt = {}
+
+function node_mt.__call (self, param)
+  assert(param and param.name, "'name' must be defined")
+  -- prepare object
+  local o = {_init__param=param}
+  setmetatable(o, self)
+  return o
+end
+
+setmetatable(Node, node_mt)
+
+local LogLevel = rclbind.LogLevel
+local sformat = string.format
+
+function Logger.info (self, ...)
+  rclbind.simp_log(LogLevel.INFO, self.name, sformat(...))
+end
+
+function Logger.warn (self, ...)
+  rclbind.simp_log(LogLevel.WARN, self.name, sformat(...))
+end
+
+function Logger.error (self, ...)
+  rclbind.simp_log(LogLevel.ERROR, self.name, sformat(...))
+end
+
+function Logger.fatal (self, ...)
+  rclbind.simp_log(LogLevel.FATAL, self.name, sformat(...))
+end
+
+function Logger.debug (self, ...)
+  rclbind.simp_log(LogLevel.DEBUG, self.name, sformat(...))
 end
 
 return Node
--- rcllua/rcllua/rcllua.lua
+++ rcllua/rcllua/rcllua.lua
@@ -1,22 +1,19 @@
 
 local rclbind = require("rcllua.rclbind")
 
+-- global
 rcllua = {}
 
-function rcllua.init (args)
-  rclbind.context_init(args)
+function rcllua.init (tbl) 
+  rclbind.context_init(tbl or arg)
 end
+
+rcllua.ok = rclbind.context_ok
+
+rcllua.shutdown = rclbind.context_shutdown
 
 function rcllua.spin (node)
   print("spin")
-end
-
-function rcllua.ok ()
-  return rclbind.context_ok()
-end
-
-function rcllua.shutdown ()
-  rclbind.context_shutdown()
 end
 
 return rcllua

c42d319778ec5528525d34ecc969b7cbc23828e6
Stanislav <mikhel.sk@gmail.com>
2025-01-22 20:30:40 +0300

    Prepare node for standard messages

---
+++ rcllua_std_msgs/cmake/rcllua_generate_interfaces.cmake
@@ -0,0 +1,157 @@
+# Copyright 2014-2018 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+macro(rcllua_generate_interfaces interface_name)
+  
+  message(WARNING ${ARGN})
+    
+  #_rosidl_cmake_register_package_hook()
+  #ament_export_dependencies(${_ARG_DEPENDENCIES})
+
+  find_package(${interface_name} REQUIRED)
+  set(_cm_dir ${${interface_name}_DIR})
+  get_filename_component(_src_path "${${interface_name}_DIR}/.." ABSOLUTE)
+  
+  
+  #get_filename_component(_msgs_path "${_cm_dir}/../msg" ABSOLUTE)
+  message(WARNING ${_src_path})
+  
+  set(_idl_tuples "")
+  set(_non_idl_tuples "")
+  set(_interface_tuples "")
+  
+  if(EXISTS "${_src_path}/msg")
+    file(GLOB _idl_files RELATIVE "${_src_path}" "${_src_path}/msg/*.idl")
+    foreach(_file ${_idl_files})
+      list(APPEND _idl_tuples "${_src_path}:${_file}")
+      list(APPEND _interface_tuples "${_src_path}:${_file}")
+    endforeach()
+    file(GLOB _msg_files RELATIVE "${_src_path}" "${_src_path}/msg/*.msg")
+    foreach(_file ${_msg_files})
+      list(APPEND _non_idl_tuples "${_src_path}:${_file}")
+      list(APPEND _interface_tuples "${_src_path}:${_file}")
+    endforeach()    
+  endif()
+  
+  #message(WARNING ${_idl_tuples})
+  
+  #message(WARNING ${_non_idl_tuples})
+  
+  # Check for any action or service interfaces
+  # Which have implicit dependencies that need to be found
+  foreach(_tuple ${_interface_tuples})
+    # We use the parent directory name to identify if the interface is an action or service
+    string(REGEX REPLACE ".*:([^:]*)$" "\\1" _tuple_file "${_tuple}")
+    get_filename_component(_parent_dir "${_tuple_file}" DIRECTORY)
+    get_filename_component(_parent_dir ${_parent_dir} NAME)
+
+    if("${_parent_dir}" STREQUAL "action")
+      # Actions depend on the packages service_msgs and action_msgs
+      find_package(service_msgs QUIET)
+      if(NOT ${service_msgs_FOUND})
+        message(FATAL_ERROR
+          "Unable to generate action interface for '${_tuple_file}'. "
+          "In order to generate action interfaces you must add a depend tag "
+          "for 'service_msgs' in your package.xml.")
+      endif()
+      ament_export_dependencies(service_msgs)
+      list_append_unique(_ARG_DEPENDENCIES "service_msgs")
+      find_package(action_msgs QUIET)
+      if(NOT ${action_msgs_FOUND})
+        message(FATAL_ERROR
+          "Unable to generate action interface for '${_tuple_file}'. "
+          "In order to generate action interfaces you must add a depend tag "
+          "for 'action_msgs' in your package.xml.")
+      endif()
+      ament_export_dependencies(action_msgs)
+      list_append_unique(_ARG_DEPENDENCIES "action_msgs")
+
+      # It is safe to break out of the loop since services only depend on service_msgs
+      # Which has already been found above
+      break()
+    elseif("${_parent_dir}" STREQUAL "srv")
+      # Services depend on service_msgs
+      find_package(service_msgs QUIET)
+      if(NOT ${service_msgs_FOUND})
+        message(FATAL_ERROR
+          "Unable to generate service interface for '${_tuple_file}'. "
+          "In order to generate service interfaces you must add a depend tag "
+          "for 'service_msgs' in your package.xml.")
+      endif()
+      ament_export_dependencies(service_msgs)
+      list_append_unique(_ARG_DEPENDENCIES "service_msgs")
+    endif()
+  endforeach()
+  
+  # collect all interface files from dependencies
+  set(_dep_files)
+  foreach(_dep ${ARGN})
+    if(NOT ${_dep}_FOUND)
+      message(FATAL_ERROR "rosidl_generate_interfaces() the passed dependency "
+        "'${_dep}' has not been found before using find_package()")
+    endif()
+    foreach(_idl_file ${${_dep}_IDL_FILES})
+      rosidl_find_package_idl(_abs_idl_file "${_dep}" "${_idl_file}")
+      list(APPEND _dep_files "${_abs_idl_file}")
+    endforeach()
+  endforeach()
+
+  # collect package names of recursive dependencies which contain interface files
+  set(_recursive_dependencies)
+  foreach(_dep ${ARGN})
+    if(DEFINED ${_dep}_IDL_FILES)
+      list_append_unique(_recursive_dependencies "${_dep}")
+    endif()
+    foreach(_dep2 ${${_dep}_RECURSIVE_DEPENDENCIES})
+      if(DEFINED ${_dep2}_IDL_FILES)
+        list_append_unique(_recursive_dependencies "${_dep2}")
+      endif()
+    endforeach()
+  endforeach()
+  
+  set(_non_idl_files "")
+  foreach(_tuple ${_non_idl_tuples})
+    string(REGEX REPLACE ":([^:]*)$" "/\\1" _non_idl_file "${_tuple}")
+    list(APPEND _non_idl_files "${_non_idl_file}")
+  endforeach()
+
+  add_custom_target(
+    ${interface_name} ALL
+    DEPENDS
+    ${_non_idl_files}
+    ${_dep_files}
+    SOURCES
+    ${_non_idl_files}
+  )
+
+  # generators must be executed in topological order
+  # which is ensured by every generator finding its dependencies first
+  # and then registering itself as an extension
+  set(rosidl_generate_interfaces_TARGET ${interface_name})
+  set(rosidl_generate_interfaces_DEPENDENCY_PACKAGE_NAMES ${_recursive_dependencies})
+  set(rosidl_generate_interfaces_LIBRARY_NAME "")
+  set(rosidl_generate_interfaces_SKIP_INSTALL "")
+  set(rosidl_generate_interfaces_ADD_LINTER_TESTS "")
+
+  set(rosidl_generate_interfaces_IDL_TUPLES ${_idl_tuples})
+
+  set(rosidl_generate_interfaces_ABS_IDL_FILES)
+  foreach(_idl_tuple ${rosidl_generate_interfaces_IDL_TUPLES})
+    string(REGEX REPLACE ":([^:]*)$" "/\\1" _abs_idl_file "${_idl_tuple}")
+    list(APPEND rosidl_generate_interfaces_ABS_IDL_FILES "${_abs_idl_file}")
+  endforeach()
+  
+  rosidl_lua_generate_lib(${interface_name}) 
+
+endmacro()
---
+++ rosidl_generator_lua/cmake/rosidl_lua_generate_lib.cmake
@@ -0,0 +1,232 @@
+# Copyright 2014-2018 Open Source Robotics Foundation, Inc.
+# Copyright 2025 Stanislav Mikhel
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+macro(rosidl_lua_generate_lib msg_name)
+  # Get a list of typesupport implementations from valid rmw implementations.
+  set(_typesupport_impls "")
+  rosidl_generator_lua_get_typesupports(_typesupport_impls)
+
+  if(_typesupport_impls STREQUAL "")
+    message(WARNING "No valid typesupport for Lua generator. Lua messages will not be generated.")
+    return()
+  endif()
+
+  set(_output_path
+    "${CMAKE_CURRENT_BINARY_DIR}/rosidl_generator_lua/${msg_name}")
+  set(_generated_c_files "")
+
+  # Collect files for each interface type
+  set(_msg_list "")
+  set(_srv_list "")
+  set(_action_list "")
+
+  foreach(_abs_idl_file ${rosidl_generate_interfaces_ABS_IDL_FILES})
+    get_filename_component(_parent_folder "${_abs_idl_file}" DIRECTORY)
+    get_filename_component(_parent_folder "${_parent_folder}" NAME)
+    get_filename_component(_idl_name "${_abs_idl_file}" NAME_WE)
+    string_camel_case_to_lower_case_underscore("${_idl_name}" _module_name)
+    set(_src_c "${_output_path}/${_parent_folder}/${_module_name}.c")
+    list(APPEND _generated_c_files ${_src_c})
+
+    # separate msg / srv / action
+    if(${_parent_folder} STREQUAL "msg")
+      list(APPEND _msg_list ${_src_c})
+    elseif(${_parent_folder} STREQUAL "srv")
+      list(APPEND _srv_list ${_src_c})
+    else()
+      list(APPEND _action_list ${_src_c})
+    endif()
+  endforeach()
+
+  # Add lua binding
+  if(NOT _msg_list STREQUAL "")
+    list(INSERT _msg_list 0 "${_output_path}/msg/msg_lib.c")
+    list(APPEND _generated_c_files "${_output_path}/msg/msg_lib.c")
+  endif()
+  if(NOT _srv_list STREQUAL "")
+    list(INSERT _srv_list 0 "${_output_path}/srv/srv_lib.c")
+    list(APPEND _generated_c_files "${_output_path}/srv/srv_lib.c")
+  endif()
+  if(NOT _action_list STREQUAL "")
+    list(INSERT _action_list 0 "${_output_path}/action/action_lib.c")
+    list(APPEND _generated_c_files "${_output_path}/action/action_lib.c")
+  endif()
+
+  file(MAKE_DIRECTORY "${_output_path}")
+
+  set(_dependency_files "")
+  set(_dependencies "")
+  foreach(_pkg_name ${rosidl_generate_interfaces_DEPENDENCY_PACKAGE_NAMES})
+    foreach(_idl_file ${${_pkg_name}_IDL_FILES})
+      set(_abs_idl_file "${${_pkg_name}_DIR}/../${_idl_file}")
+      normalize_path(_abs_idl_file "${_abs_idl_file}")
+      list(APPEND _dependency_files "${_abs_idl_file}")
+      list(APPEND _dependencies "${_pkg_name}:${_abs_idl_file}")
+    endforeach()
+  endforeach()
+
+  set(target_dependencies
+    "${rosidl_generator_lua_BIN}"
+    ${rosidl_generator_lua_GENERATOR_FILES}
+    "${rosidl_generator_lua_TEMPLATE_DIR}/idl.c.em"
+    "${rosidl_generator_lua_TEMPLATE_DIR}/msg.c.em"
+    ${rosidl_generate_interfaces_ABS_IDL_FILES}
+    ${_dependency_files})
+  foreach(dep ${target_dependencies})
+    if(NOT EXISTS "${dep}")
+      message(FATAL_ERROR "Target dependency '${dep}' does not exist")
+    endif()
+  endforeach()
+
+  set(generator_arguments_file "${CMAKE_CURRENT_BINARY_DIR}/rosidl_generator_lua__${msg_name}.json")
+  rosidl_write_generator_arguments(
+    "${generator_arguments_file}"
+    PACKAGE_NAME "${msg_name}"
+    IDL_TUPLES "${rosidl_generate_interfaces_IDL_TUPLES}"
+    ROS_INTERFACE_DEPENDENCIES "${_dependencies}"
+    OUTPUT_DIR "${_output_path}"
+    TEMPLATE_DIR "${rosidl_generator_lua_TEMPLATE_DIR}"
+    TARGET_DEPENDENCIES ${target_dependencies}
+  )
+
+  set(_target_suffix "__lua")
+
+  set_property(
+    SOURCE ${_generated_c_files}
+    PROPERTY GENERATED 1
+  )
+
+  # Set generation rule
+  add_custom_command(
+    OUTPUT ${_generated_c_files}
+    COMMAND Python3::Interpreter
+    ARGS ${rosidl_generator_lua_BIN}
+    --generator-arguments-file "${generator_arguments_file}"
+    --typesupport-impls "${_typesupport_impls}"
+    DEPENDS ${target_dependencies}
+    COMMENT "Generating Lua code for ROS interfaces"
+    VERBATIM
+  )
+
+  if(TARGET ${rosidl_generate_interfaces_TARGET}${_target_suffix})
+    message(WARNING "Custom target ${rosidl_generate_interfaces_TARGET}${_target_suffix} already exists")
+  else()
+    add_custom_target(
+      ${rosidl_generate_interfaces_TARGET}${_target_suffix}
+      DEPENDS
+      ${_generated_c_files})
+  endif()
+
+  # Export target so downstream interface packages can link to it
+  set(rosidl_generator_lua_suffix "__rosidl_generator_lua")
+
+  set(_target_name_lib "${rosidl_generate_interfaces_TARGET}${rosidl_generator_lua_suffix}")
+
+  add_library(${_target_name_lib} SHARED ${_generated_c_files})
+  ament_target_dependencies(${_target_name_lib} 
+    rosidl_runtime_c
+    ${msg_name}
+    rosidl_luacommon
+  )
+#  target_link_libraries(${_target_name_lib}
+#    ${rosidl_generate_interfaces_TARGET}__rosidl_generator_c
+#    ${rosidl_generate_interfaces_TARGET}__rosidl_typesupport_c
+#  )
+#  add_dependencies(
+#    ${_target_name_lib}
+#    ${rosidl_generate_interfaces_TARGET}${_target_suffix}
+#    #${rosidl_generate_interfaces_TARGET}__rosidl_typesupport_c
+#  )
+  
+
+  # Additional definitions
+  set(_luacommon_dir "${rosidl_luacommon_DIR}/../../../include")
+  normalize_path(_luacommon_dir "${_luacommon_dir}")
+
+  target_include_directories(${_target_name_lib}
+    PRIVATE
+    ${CMAKE_CURRENT_BINARY_DIR}/rosidl_generator_c
+  )
+
+  rosidl_get_typesupport_target(c_typesupport_target 
+    "${rosidl_generate_interfaces_TARGET}" "rosidl_typesupport_c")
+
+  # Compile Lua msgs
+  if(NOT _msg_list STREQUAL "")
+    add_library(msg__${msg_name} SHARED ${_msg_list})
+    set_target_properties(msg__${msg_name} PROPERTIES
+      PREFIX ""
+      LIBRARY_OUTPUT_NAME msg
+      LIBRARY_OUTPUT_DIRECTORY ${_output_path}
+    )    
+    ament_target_dependencies(msg__${msg_name}
+      rosidl_runtime_c
+      rosidl_luacommon
+      ${msg_name}
+    )
+    target_link_libraries(msg__${msg_name}
+      #${c_typesupport_target}
+      #${rosidl_generate_interfaces_TARGET}__rosidl_typesupport_c
+      #${rosidl_generate_interfaces_TARGET}__rosidl_generator_c
+      rosidl_luacommon::rosidl_utility
+    )
+  endif()
+
+  # Compile Lua srvs
+  if(NOT _srv_list STREQUAL "")
+    add_library(srv SHARED ${_srv_list})
+    set_target_properties(srv PROPERTIES
+      PREFIX ""
+      LIBRARY_OUTPUT_DIRECTORY ${_output_path}
+    )
+    target_link_libraries(srv
+      ${c_typesupport_target}
+      rosidl_luacommon::rosidl_utility
+    )
+    ament_target_dependencies(srv
+      "rosidl_runtime_c"
+      "rosidl_luacommon"
+    )
+  endif()
+
+  # Compile Lua actions
+  if(NOT _action_list STREQUAL "")
+    add_library(action SHARED ${_action_list})
+    set_target_properties(action PROPERTIES
+      PREFIX ""
+      LIBRARY_OUTPUT_DIRECTORY ${_output_path}
+    )
+    target_link_libraries(action
+      ${rosidl_generate_interfaces_TARGET}__rosidl_generator_c
+      rosidl_luacommon::rosidl_utility
+    )
+    target_include_directories(action PRIVATE
+      ${CMAKE_CURRENT_BINARY_DIR}/rosidl_generator_c
+      ${_luacommon_dir})
+  endif()
+
+  # Install
+  if(NOT _msg_list STREQUAL "")
+    rcllua_cmake_install_clib(${msg_name} msg__${msg_name})
+  endif()
+  if(NOT _srv_list STREQUAL "")
+    rcllua_cmake_install_clib(${msg_name} srv)
+  endif()
+  if(NOT _action_list STREQUAL "")
+    rcllua_cmake_install_clib(${msg_name} action)
+  endif()
+endmacro()
+
